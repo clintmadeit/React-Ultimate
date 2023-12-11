49046078f9c472c33a68a73f6e3b050d35ccec84
clintmadeit <clintonbrirdley@gmail.com>
2023-12-11 17:59:06 +0300

    Adding menu description

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -73,6 +73,11 @@ function Menu() {
     <main className="menu">
       <h2>Our Menu</h2>
 
+      <p>
+        Authentic Italian cuisine. 6 creative dishes to choose from. All from
+        our stone oven, all organic, all delicious.
+      </p>
+
       {numPizzas > 0 ? (
         <ul className="pizzas">
           {pizzaData.map((pizza) => (

fe2eb6b8c9b7aec5c0483a4f2ed912f882234508
clintmadeit <clintonbrirdley@gmail.com>
2023-12-11 18:23:47 +0300

    react fragment

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -73,17 +73,18 @@ function Menu() {
     <main className="menu">
       <h2>Our Menu</h2>
 
-      <p>
-        Authentic Italian cuisine. 6 creative dishes to choose from. All from
-        our stone oven, all organic, all delicious.
-      </p>
-
       {numPizzas > 0 ? (
-        <ul className="pizzas">
-          {pizzaData.map((pizza) => (
-            <Pizza pizzaObj={pizza} key={pizza.name} />
-          ))}
-        </ul>
+        <>
+          <p>
+            Authentic Italian cuisine. 6 creative dishes to choose from. All
+            from our stone oven, all organic, all delicious.
+          </p>
+          <ul className="pizzas">
+            {pizzaData.map((pizza) => (
+              <Pizza pizzaObj={pizza} key={pizza.name} />
+            ))}
+          </ul>
+        </>
       ) : (
         <p>
           We're currently working on our menu. Please come back in a few 😃.

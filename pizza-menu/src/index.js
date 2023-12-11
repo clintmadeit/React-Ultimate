b9bfd7effa3b69e14475f07e6306b1802993774f
clintmadeit <clintonbrirdley@gmail.com>
2023-12-11 16:59:37 +0300

    Dynamically displaying pizzas array conditionally with ternaries

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -67,18 +67,22 @@ function Header() {
 
 function Menu() {
   const pizzas = pizzaData;
-  // const pizzas = []
+  // const pizzas = [];
   const numPizzas = pizzas.length;
   return (
     <main className="menu">
       <h2>Our Menu</h2>
 
-      {numPizzas > 0 && (
+      {numPizzas > 0 ? (
         <ul className="pizzas">
           {pizzaData.map((pizza) => (
             <Pizza pizzaObj={pizza} key={pizza.name} />
           ))}
         </ul>
+      ) : (
+        <p>
+          We're currently working on our menu. Please come back in a few 😃.
+        </p>
       )}
 
       {/* <Pizza
@@ -124,11 +128,15 @@ function Footer() {
   // }
   return (
     <footer className="footer">
-      {isOpen && (
+      {isOpen ? (
         <div className="order">
           <p>We're open until {closeHour}:00. Come visit us or order online.</p>
           <button className="btn">Order</button>
         </div>
+      ) : (
+        <p>
+          We're happy to welcome you between {openHour}:00 and {closeHour}:00
+        </p>
       )}
     </footer>
   );

5645467ed0f99677da503ee80c24b98abc804d18
clintmadeit <clintonbrirdley@gmail.com>
2023-12-12 18:55:25 +0300

    steps functionality using state

--- steps/src/App.js
+++ steps/src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const messages = [
   "Learn React 😀",
   "Apply for jobs 😄",
@@ -5,22 +7,28 @@ const messages = [
 ];
 
 export default function App() {
-  const step = 2;
+  const [step, setStep] = useState(1);
 
   function handlePrevious() {
-    alert("Previous");
+    if (step > 1) setStep(step - 1);
+    else {
+      alert("You are at step 0. Please click next.");
+    }
   }
 
   function handleNext() {
-    alert("Next");
+    if (step < 3) setStep(step + 1);
+    else {
+      alert("You are at the last step. Please click previous.");
+    }
   }
 
   return (
     <div className="step">
       <div className="numbers">
-        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
-        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
-        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+        <div className={step >= 1 ? "active" : ""}>1</div>
+        <div className={step >= 2 ? "active" : ""}>2</div>
+        <div className={step >= 3 ? "active" : ""}>3</div>
       </div>
 
       <p className="message">

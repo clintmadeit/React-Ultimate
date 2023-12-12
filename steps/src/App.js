a041705cebcb51fddde828ed717175efb0d68004
clintmadeit <clintonbrirdley@gmail.com>
2023-12-12 18:12:05 +0300

    dynamic classes

--- steps/src/App.js
+++ steps/src/App.js
@@ -1,7 +1,31 @@
+const messages = [
+  "Learn React 😀",
+  "Apply for jobs 😄",
+  "Invest in your income 😛",
+];
+
 export default function App() {
+  const step = 2;
   return (
-    <>
-      <h1>Hello React !</h1>
-    </>
+    <div className="step">
+      <div className="numbers">
+        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+      </div>
+
+      <p className="message">
+        Step {step}: {messages[step - 1]}
+      </p>
+
+      <div className="buttons">
+        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+          Previous
+        </button>
+        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+          Next
+        </button>
+      </div>
+    </div>
   );
 }

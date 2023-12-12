2f2c667836412f589197ac783872bc6e4520a1b6
clintmadeit <clintonbrirdley@gmail.com>
2023-12-12 23:24:49 +0300

    styling

--- steps/src/App.js
+++ steps/src/App.js
@@ -28,30 +28,32 @@ export default function App() {
         &times;
       </button>
       {isOpen && (
-        <div className="step">
-          <div className="numbers">
-            <div className={step >= 1 ? "active" : ""}>1</div>
-            <div className={step >= 2 ? "active" : ""}>2</div>
-            <div className={step >= 3 ? "active" : ""}>3</div>
-          </div>
+        <div className="container">
+          <div className="step">
+            <div className="numbers">
+              <div className={step >= 1 ? "active" : ""}>1</div>
+              <div className={step >= 2 ? "active" : ""}>2</div>
+              <div className={step >= 3 ? "active" : ""}>3</div>
+            </div>
 
-          <p className="message">
-            Step {step}: {messages[step - 1]}
-          </p>
+            <p className="message">
+              Step {step}: {messages[step - 1]}
+            </p>
 
-          <div className="buttons">
-            <button
-              style={{ backgroundColor: "#7950f2", color: "#fff" }}
-              onClick={handlePrevious}
-            >
-              Previous
-            </button>
-            <button
-              style={{ backgroundColor: "#7950f2", color: "#fff" }}
-              onClick={handleNext}
-            >
-              Next
-            </button>
+            <div className="buttons">
+              <button
+                style={{ backgroundColor: "#7950f2", color: "#fff" }}
+                onClick={handlePrevious}
+              >
+                Previous
+              </button>
+              <button
+                style={{ backgroundColor: "#7950f2", color: "#fff" }}
+                onClick={handleNext}
+              >
+                Next
+              </button>
+            </div>
           </div>
         </div>
       )}

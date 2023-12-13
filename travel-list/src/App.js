4ee72f6537cd53364d608a1dcc4f833cfe004aab
clintmadeit <clintonbrirdley@gmail.com>
2023-12-13 20:36:10 +0300

    updates on the main app

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -2,8 +2,35 @@ import "./index.css";
 
 export default function App() {
   return (
-    <div>
-      <h1>Hello React</h1>
+    <div className="app">
+      <Logo />
+      <Form />
+      <ParkingList />
+      <Stats />
     </div>
+  );
+}
+
+function Logo() {
+  return <h1>🌴 Far Away 👜</h1>;
+}
+
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your 😍trip?</h3>
+    </div>
+  );
+}
+
+function ParkingList() {
+  return <div className="list">LIST</div>;
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>👜 You have x items on your list, and you already parked x (x%)</em>
+    </footer>
   );
 }

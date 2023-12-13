2e6c612f58709fdd4e58f363aafe06a1105a9363
clintmadeit <clintonbrirdley@gmail.com>
2023-12-13 22:39:50 +0300

    Building form and handling submission

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -1,4 +1,10 @@
 import "./index.css";
+import { CgClose } from "react-icons/cg";
+
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+];
 
 export default function App() {
   return (
@@ -16,15 +22,48 @@ function Logo() {
 }
 
 function Form() {
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍trip?</h3>
-    </div>
+      <select>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input type="text" placeholder="item..." />
+      <button>Add</button>
+    </form>
   );
 }
 
 function ParkingList() {
-  return <div className="list">LIST</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} key={item.id} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button style={{ color: "red", fontSize: "inherit" }}>
+        {<CgClose />}
+      </button>
+    </li>
+  );
 }
 
 function Stats() {

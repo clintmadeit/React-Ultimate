964ff0584225ab4259a3b3e7c358c023facc86e5
clintmadeit <clintonbrirdley@gmail.com>
2023-12-15 16:30:00 +0300

    lifting state up

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import "./index.css";
+import "./styles.css";
 import { CgClose } from "react-icons/cg";
 
 const initialItems = [
@@ -8,11 +8,16 @@ const initialItems = [
 ];
 
 export default function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
   return (
     <div className="app">
       <Logo />
-      <Form />
-      <ParkingList />
+      <Form onAddItems={handleAddItems} />
+      <ParkingList items={items} />
       <Stats />
     </div>
   );
@@ -22,7 +27,7 @@ function Logo() {
   return <h1>🌴 Far Away 👜</h1>;
 }
 
-function Form() {
+function Form({ onAddItems }) {
   const [description, setDescription] = useState("");
   const [quantity, setQuantity] = useState(1);
 
@@ -32,7 +37,8 @@ function Form() {
     if (!description) return;
 
     const newItem = { description, quantity, package: false, id: Date.now() };
-    console.log(newItem);
+
+    onAddItems(newItem);
 
     setDescription("");
     setQuantity(1);
@@ -61,11 +67,11 @@ function Form() {
   );
 }
 
-function ParkingList() {
+function ParkingList({ items }) {
   return (
     <div className="list">
       <ul>
-        {initialItems.map((item) => (
+        {items.map((item) => (
           <Item item={item} key={item.id} />
         ))}
       </ul>
